9f2b41c90282f812cb076a57bac86cd0c0a528a2
Vinicius Solon <vini.97@live.com>
2021-09-28 20:46:58 -0300

    Implementação do microserviço hr-worker

---
+++ hr-worker/src/main/java/com/vinisolon/hrworker/entities/Worker.java
@@ -0,0 +1,31 @@
+package com.vinisolon.hrworker.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
---
+++ hr-worker/src/main/java/com/vinisolon/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.vinisolon.hrworker.repositories;
+
+import com.vinisolon.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
