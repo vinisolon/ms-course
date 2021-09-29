66489a77e0e6db8e859bc233f3a32561b099633e
Vinicius Solon <vini.97@live.com>
2021-09-28 22:04:33 -0300

    Implementado comunicação entre microserviços utilizando feign client

--- hr-payroll/src/main/java/com/vinisolon/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/vinisolon/hrpayroll/services/PaymentService.java
@@ -2,28 +2,18 @@ package com.vinisolon.hrpayroll.services;
 
 import com.vinisolon.hrpayroll.entities.Payment;
 import com.vinisolon.hrpayroll.entities.Worker;
+import com.vinisolon.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}") // Pega o valor desse atributo no application.properties
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient feign;
 
     public Payment getPayment(Long workerId, Integer days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
-        String url = workerHost + "/workers/{id}";
-        Worker obj = restTemplate.getForObject(url, Worker.class, uriVariables);
+        Worker obj = feign.findById(workerId).getBody();
         return new Payment(obj.getName(), obj.getDailyIncome(), days);
     }
 
