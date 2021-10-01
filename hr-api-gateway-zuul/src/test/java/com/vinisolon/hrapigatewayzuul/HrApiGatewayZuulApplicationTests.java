f9ee05be39a879a9dfbf17019fb5f24488e28852
Vinicius Solon <vini.97@live.com>
2021-10-01 05:38:28 -0300

    Criação do projeto hr-api-gateway-zuul

---
+++ hr-api-gateway-zuul/src/test/java/com/vinisolon/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisolon.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
