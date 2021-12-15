20066b008813eeb47e1b611f673b1f46b5a17abc
Vinicius Solon <vini.97@live.com>
2021-12-15 14:08:59 -0300

    tentativa de consertar servidor de configuração em repositório git privado utilizando ssh key como validação

--- hr-config-server/src/main/java/com/vinisolon/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/vinisolon/hrconfigserver/HrConfigServerApplication.java
@@ -14,14 +14,14 @@ public class HrConfigServerApplication implements CommandLineRunner {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
-	@Value("${spring.cloud.config.server.git.username}")
-	private String gitUsername;
-	@Value("${spring.cloud.config.server.git.password}")
+//	@Value("${spring.cloud.config.server.git.username}")
+//	private String gitUsername;
+	@Value("${spring.cloud.config.server.git.passphrase}")
 	private String gitPassword;
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("GIT USERNAME: " + gitUsername);
-		System.out.println("GIT PASSWORD: " + gitPassword);
+//		System.out.println("GIT USERNAME: " + gitUsername);
+		System.out.println("GIT PASSPHRASE: " + gitPassword);
 	}
 }
