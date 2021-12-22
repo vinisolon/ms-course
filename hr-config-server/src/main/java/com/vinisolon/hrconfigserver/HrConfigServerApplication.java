557a8bc505be39de092e5dadb902d93674dcbf1c
Vinicius Solon <vini.97@live.com>
2021-12-22 19:02:23 -0300

    Organizando arquivos de configuração

--- hr-config-server/src/main/java/com/vinisolon/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/vinisolon/hrconfigserver/HrConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.vinisolon.hrconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,17 +13,7 @@ public class HrConfigServerApplication implements CommandLineRunner {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
-//	@Value("${spring.cloud.config.server.git.username}")
-//	private String gitUsername;
-//	@Value("${spring.cloud.config.server.git.passphrase}")
-//	private String gitPassword;
-//	@Value("${spring.cloud.config.server.git.private-key}")
-//	private String privateKey;
-
 	@Override
 	public void run(String... args) throws Exception {
-//		System.out.println("GIT USERNAME: " + gitUsername);
-//		System.out.println("GIT PASSPHRASE: " + gitPassword);
-//		System.out.println("PRIVATE-KEY: " + privateKey);
 	}
 }
