7e1928c20a7fe773244802cec83c94576cf7643c
Vinicius Solon <vini.97@live.com>
2021-12-27 14:03:49 -0300

    Criado projeto hr-user responsavel pelos logins e senhas dos usuários da rede de microserviços

---
+++ hr-user/src/test/java/com/vinisolon/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisolon.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
